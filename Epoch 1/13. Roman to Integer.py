1102ef594287ba36f1a09f2ae189300233561eee
ShuChenDev <shu.chen.xm.work@gmail.com>
2024-09-03 19:46:23 -0400

    Create 13. Roman to Integer.py

---
+++ Epoch 1/13. Roman to Integer.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def romanToInt(self, s):
+        dic = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+
+        k = len(s)
+
+        sum = dic[s[0]]
+        for i in range(1, k):
+            if dic[s[i]] > dic[s[i - 1]]:
+                sum -= 2* dic[s[i - 1]]
+            sum += dic[s[i]]
+
+        return sum
