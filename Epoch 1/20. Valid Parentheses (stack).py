ee18c9db45f26b195a0fd1800a761ef2abd4f2cc
ShuChenDev <shu.chen.xm.work@gmail.com>
2024-09-07 11:31:53 -0400

    Create 20. Valid Parentheses (stack).py

---
+++ Epoch 1/20. Valid Parentheses (stack).py
@@ -0,0 +1,18 @@
+"""
+9/7/2024
+stack
+"""
+class Solution(object):
+    def isValid(self, s):
+        x = len(s)
+        if x % 2 == 1 or x == 0:
+            return False
+        else:
+            pairs = {'(':')', '[':']', '{':'}'}
+            stack = []
+            for i in s:
+                if i in pairs: #open bracket
+                    stack.append(i)
+                elif len(stack) == 0 or i != pairs[stack.pop()]: #closing bracket
+                    return False
+            return len(stack) == 0
