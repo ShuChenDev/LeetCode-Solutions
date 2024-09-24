c5ee5a8de69af21c7a7f2aaa2598ec62a02e1384
ShuChenDev <shu.chen.xm.work@gmail.com>
2024-09-24 09:50:41 -0400

    Create 258. Add Digits (While Loops).py

---
+++ Epoch 1/258. Add Digits (While Loops).py
@@ -0,0 +1,15 @@
+"""
+9/24/2024
+While Loops
+"""
+
+class Solution(object):
+    def addDigits(self, num):
+        
+        while num >= 10:
+            temp = 0
+            while num > 0:
+                temp += num % 10
+                num = num // 10
+            num = temp
+        return num
