c29159a9ffc1ca6ffcad89387132a29ba5d44f61
ShuChenDev <shu.chen.xm.work@gmail.com>
2024-09-25 08:56:05 -0400

    Create 278. First Bad Version (Binary Search).py

---
+++ Epoch 1/278. First Bad Version (Binary Search).py
@@ -0,0 +1,17 @@
+"""
+9/25/2024
+Binary Search
+"""
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        start = 1
+        end = n
+
+        while start < end:
+            mid = (start + end) // 2
+            if isBadVersion(mid):
+                end = mid
+            else:
+                start = mid + 1
+        return start
