c5147cce94215c832df62a007616aaf0f6008b35
ShuChenDev <shu.chen.xm.work@gmail.com>
2024-11-13 09:35:33 -0500

    Create 169. Majority Element.cpp

---
+++ Epoch 2 (C++)/Math/169. Majority Element.cpp
@@ -0,0 +1,12 @@
+/*
+11/13/2024
+Math
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
+    }
+};
