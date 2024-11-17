e312e093f0966232767a7be7726ed5425d6b9842
ShuChenDev <shu.chen.xm.work@gmail.com>
2024-11-17 17:20:57 -0500

    Create 219. Contains Duplicate II.cpp

---
+++ Epoch 2 (C++)/Hash Map/219. Contains Duplicate II.cpp
@@ -0,0 +1,23 @@
+/*
+11/17/2024
+Hash Map
+*/
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> seen;
+        for(int i = 0; i < nums.size(); i++) {
+            if(seen.find(nums[i]) != seen.end() && i - seen[nums[i]] <= k) {
+                return true;
+            }
+            seen[nums[i]] = i;
+        }
+
+
+
+        return false;
+        
+    }
+};
