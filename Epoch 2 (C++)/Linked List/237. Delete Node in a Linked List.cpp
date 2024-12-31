82b9fa0820c139730ea7f5048e768d56079ad6cc
ShuChenDev <shu.chen.xm.work@gmail.com>
2024-12-31 10:22:07 -0500

    Create 237. Delete Node in a Linked List.cpp

---
+++ Epoch 2 (C++)/Linked List/237. Delete Node in a Linked List.cpp
@@ -0,0 +1,12 @@
+/*
+Linked List
+12/31/2024
+*/
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+       node->val = node->next->val;
+       node->next = node->next->next; 
+    }
+};
